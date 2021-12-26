989ce87742a0d0da3aade4fcc888edaecf06069c
Hugo <hpjm37@gmail.com>
2021-12-26 21:21:51 +0000

    Make FEx 'use'able

--- lib/fex.ex
+++ lib/fex.ex
@@ -1,18 +1,43 @@
 defmodule FEx do
   @moduledoc """
-  Documentation for `FEx`.
+  Wrapper over FX APIs.
   """
 
-  @doc """
-  Hello world.
+  defmacro __using__(opts \\ []) do
+    api_module = opts |> Keyword.fetch!(:api) |> Macro.expand_once(__ENV__)
 
-  ## Examples
+    callbacks = FEx.APIs.behaviour_info(:callbacks)
+    {:ok, docs} = get_docs(api_module)
 
-      iex> FEx.hello()
-      :world
+    for {fun, arity} <- callbacks do
+      {signature, doc} =
+        Enum.find_value(docs, fn
+          {{:function, ^fun, ^arity}, _, [signature], %{"en" => doc}, _} -> {signature, doc}
+          _ -> false
+        end)
 
-  """
-  def hello do
-    :world
+      [_all, args] = Regex.run(~r|\(([^)]*)\)|, signature)
+
+      args =
+        args
+        |> String.split(", ")
+        |> Enum.reject(&(&1 == ""))
+        |> Enum.map(&String.to_existing_atom/1)
+        |> Enum.map(&{&1, [], __MODULE__})
+
+      quote do
+        @doc unquote(doc)
+        def unquote(fun)(unquote_splicing(args)) do
+          unquote(api_module).unquote(fun)(unquote_splicing(args))
+        end
+      end
+    end
+  end
+
+  defp get_docs(api_module) do
+    case Code.fetch_docs(api_module) do
+      {_, _, _, _, _, _, docs} -> {:ok, docs}
+      {:error, _} = error -> error
+    end
   end
 end
--- test/fex_test.exs
+++ test/fex_test.exs
@@ -2,7 +2,20 @@ defmodule FExTest do
   use ExUnit.Case
   doctest FEx
 
-  test "greets the world" do
-    assert FEx.hello() == :world
+  import Mox
+
+  setup :verify_on_exit!
+
+  defmodule ExampleUsing do
+    use FEx, api: FEx.APIs.Fixer
+  end
+
+  describe "use'ing module injects functions into caller which defer to given api" do
+    test "with example module" do
+      expected = %{from: :GBP, rate: 1.0, to: :USD}
+      expect(FEx.APIs.Fixer.Mock, :rate, fn :GBP, :USD -> expected end)
+
+      assert ExampleUsing.rate(:GBP, :USD) == expected
+    end
   end
 end
